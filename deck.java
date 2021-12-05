b44b51e201284ebe1a86e537af7fd8b86706d7d6
Markita Daniel <93155332+MDaniel89@users.noreply.github.com>
2021-12-04 20:28:28 -0600

    Create deck.java

---
+++ deck.java
@@ -0,0 +1,32 @@
+package Week6_FinalProject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author markitadaniel
+ *
+ */
+public class Deck {
+	
+	
+List<Card> cards = new ArrayList<Card>();
+
+public Deck() {
+	for (int i = 2; i < 15; i++) {
+		cards.add(new Card (i, "Diamonds"));
+		cards.add(new Card (i, "Hearts"));
+		cards.add(new Card (i, "Clubs"));
+		cards.add(new Card (i, "Spades"));}
+	}
+	
+	
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+	
+	public Card draw() {
+		return cards.remove(0);
+	}
+}
